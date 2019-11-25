89aa488ee854f66ebb1c550653c5af8f07b50bb6
JustinYeung-source <56745018+JustinYeung-source@users.noreply.github.com>
2019-11-25 13:46:30 -0700

    added api first 1000 due to bug
    read title

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 module edmontonAssessment {
 	opens ca.macewan.cmpt305 to javafx.graphics;
 	requires org.junit.jupiter.api;
@@ -8,15 +8,4 @@ module edmontonAssessment {
 	requires javafx.web;
 	requires java.base;
 	requires java.json;
-=======
-module edmontonAssessment {
-	opens ca.macewan.cmpt305 to javafx.graphics;
-	requires org.junit.jupiter.api;
-	requires javafx.graphics;
-	requires javafx.controls;
-	requires javafx.base;
-	requires javafx.web;
-	requires java.base;
-	requires java.json;
->>>>>>> Stashed changes
 }
