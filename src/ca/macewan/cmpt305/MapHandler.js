1a0f79ffbf9ac45a5cee904eb065fe90900e4e0b
Rozario B <r_beaudin@outlook.com>
2019-11-25 14:47:07 -0700

    added styles to website
    read title

---
+++ src/ca/macewan/cmpt305/MapHandler.js
@@ -0,0 +1,19 @@
+/* CMPT 305 Milestone 3
+*  Edmonton Properties assessment 
+*/
+
+
+main();
+
+function main(){
+    
+}
+
+
+/**
+ * 
+ * @param {List of Properties} listData 
+ */
+function getDataFromJava(listData){
+
+}
