7cc3c6d1fb02cf2ae8602eb3a648dea45308a150
Rozario B <r_beaudin@outlook.com>
2019-11-26 15:29:06 -0700

    Added framework for the fetching in the javascript
    Note that this is only a temporary fix until we get the filtered list to json functionality working. And I will finish up the google maps stuff when I get home after my workout with my friend.

--- src/ca/macewan/cmpt305/MapHandler.js
+++ src/ca/macewan/cmpt305/MapHandler.js
@@ -6,25 +6,51 @@
 main();
 
 function main(){
-    var json = document.getElementById("PropertyAssessmentGUI");
-    console.log(json)
-    var latitudeList = getDataFromJava(document.getElementById("PropertyAssessmentGUI"));
-    console.log(latitudeList);
+    // var javaFXClass = document.getElementById("PropertyAssessmentGUI");
+    // var theJson = javaFXClass.getPropertyAPI("https://data.edmonton.ca/resource/q7d6-ambg.json");
+    // console.log(theJson);
+    var request = new XMLHttpRequest();
+    //var javafx = new PropertyAssessmentGUI();
+    var latitudeList = [];
+    var longitudeList = [];
+    var assessmentList = [];
+    request.open('GET', 'https://data.edmonton.ca/resource/q7d6-ambg.json');
+    
+    request.onload = function(){
+        var data = JSON.parse(this.response);
+
+        if(request.status > 199 && request.status < 400){
+            data.forEach(element => {
+                latitudeList.push(element.latitude);
+                longitudeList.push(element.longitude);
+                assessmentList.push(element.total_asmt);
+                
+            });
+        }
+        else{
+            console.error("Error the request for the API could not be made.");
+        }
+
+        
+
+    };
+
+    request.send()
 }
 
 
-/**
- * 
- * @param {List of Properties} jsonName 
- */
-function getDataFromJava(jsonName){
-    if(typeof jsonName == "undefined"){
-        printError("JSON name has not been defined.");
-    }
-    var dataList = [];
-    for(let i = 0; i < jsonName.length; i++){
-        dataList.push(jsonName[i].latitude);
-        console.log(jsonName[i]);
-    }
-    return dataList;
-}
+// /**
+//  * 
+//  * @param {List of Properties} jsonName 
+//  */
+// function getDataFromJava(jsonName){
+//     // if(typeof jsonName == "undefined"){
+//     //     printError("JSON name has not been defined.");
+//     // }
+//     // var dataList = [];
+//     // for(let i = 0; i < jsonName.length; i++){
+//     //     dataList.push(jsonName[i].latitude);
+//     //     console.log(jsonName[i]);
+//     // }
+//     return dataList;
+// }
