07cc3d71a40686fd61e50baf3ae247174f1718ee
JustinYeung-source <56745018+JustinYeung-source@users.noreply.github.com>
2019-12-03 13:35:22 -0700

    Fixed everything and everything works
    Just need comments and padding for that bra

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,10 @@
-
 module edmontonAssessment {
-	opens ca.macewan.cmpt305 to javafx.graphics;
-	requires org.junit.jupiter.api;
-	requires javafx.graphics;
-	requires javafx.controls;
+	opens ca.macewan.cmpt305;
+	requires java.json;
 	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
 	requires javafx.web;
 	requires java.base;
-	requires java.json;
+	requires org.junit.jupiter.api;
 }
